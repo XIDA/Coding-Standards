d3f02d5519357b82db7e923fc8816db91c5c6c23
BenjaminKobjolke <spamprotect@xida.de>
2014-11-01 00:39:13 +0100

    improved publishing

--- _publish/convert.bat
+++ _publish/convert.bat
@@ -1,5 +1,9 @@
 rmdir /Q /S tmp
 xcopy ..\*.md tmp\
+xcopy ..\filestructures tmp\filestructures\
+xcopy ..\general tmp\general\
 fnr.exe --cl --dir "%~dp0\tmp" --fileMask "*.md" --excludeFileMask "*.dll, *.exe" --includeSubDirectories --useRegEx --find "\[.*\].*\(.*\.md\)" --replace ""
-
-pandoc --toc -V documentclass=report -f markdown -t latex -o xd_codingstandards_v0.1.pdf README.md Definitions.md "SVN Commit guidelines.md" "How to prepare your code.md"
+cd tmp
+pandoc --toc -V documentclass=report -f markdown -t latex -o ..\xd_codingstandards_v0.1.pdf README.md Definitions.md Styling.md "SVNCommitGuidelines.md" "HowToPrepareYourCode.md" "general\README.md" "general\AvoidComplicatedIfs.md" "filestructures\README.md" "filestructures\Components.md" "filestructures\Delegates.md" "filestructures\FileAssets.md" "filestructures\Libraries.md" "filestructures\Managers.md"
+cd ..
+rmdir /Q /S tmp
